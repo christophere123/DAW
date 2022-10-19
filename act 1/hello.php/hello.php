377f09e0ac41d88e011332022fc114ce7d983cf7
christophere123 <114452953+christophere123@users.noreply.github.com>
2022-10-18 19:18:18 -0500

    Add files via upload

---
+++ act 1/hello.php/hello.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Buenos Dias
+	</title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body >
+
+<center><br><br><marquee bgcolor=#2BE9CF  width=900 height=110 behavior=alternate direction=left><font size=5 color="black"><h1>Bienvenido a mi pagina</h1></marquee></font></center>
+<div id="general">
+<center><br>
+<?php
+echo "<h1>Hola a todos</h1>";
+
+echo "<h3>Bienvenidos al mundo dep php</h3>";
+
+echo "<h3>Soy hackiman</h3>";
+?>
+
+
+<a href="https://youtu.be/Nga0KbftHF8"><img src="vip.jpg" width="600" height="300"></a>
+</div></center>
+</div>
+</body>
+</html>
